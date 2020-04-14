a1cdf768140dd85e25809ee4bfb520c9bcd58d76
iran27 <iransantosjr@gmail.com>
2020-04-14 16:36:39 -0300

    troca

--- t9.py
+++ t9.py
@@ -26,6 +26,6 @@ def func_primos():
     return str(lista)
 
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
