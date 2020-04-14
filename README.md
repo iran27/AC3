60261c8e3ed21797648af427c2fd36ee50ad2738
iran27 <iransantosjr@gmail.com>
2020-04-14 01:28:04 -0300

    AC3

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AC3
+Hoje eh aula de heroku
+
+git init git add. git commit git remote add origin https://github.com/iran27/AC3.git git push -u origem mestre
---
+++ t9.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask,jsonify,request
+from math import sqrt
+
+app = Flask(_name_)
+
+
+@app.route('/')
+def func_primo():
+
+    cont = 0
+    num = 2
+    lista = []
+    while (cont <= 100):
+        normal = False
+        for i in range(2, num):
+            if (num % i == 0):
+                normal = True
+                break
+            if (not normal):
+                cont += 1
+                lista.append(num)
+
+            num += 1
+
+    return str(lista)
+
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port = port)
